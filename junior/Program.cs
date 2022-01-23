7434e90a2eafd3735fa02b29e1be0a898bc1f91d
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-01-23 21:19:00 +0300

    dev 1
    первое домашнее задание 1.1

---
+++ junior/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace junior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Agnes";
+            sbyte level = 1;
+            byte age = 18;
+            float money = 10.99f;
+            double distance = 100.11;
+            uint progress = 55;
+            long countDeath = 150;
+            short countCrystals = 10;
+            int countFriends = 15;
+            bool isAdult = true;
+
+        }
+    }
+}
