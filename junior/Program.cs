e94576677c6a80907b202a5afff4fa406f505fe5
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-01-25 20:31:40 +0300

    dew 1
    урок 9

--- junior/Program.cs
+++ junior/Program.cs
@@ -11,20 +11,84 @@ namespace junior
 
         static void Main(string[] args)
         {
-            Random random = new Random();
-            string userInput;
-            bool isExit = false;
+            float walletRub = 100;
+            float walletUsd = 0;
+            float walletEur = 0;
+            float walletUa = 0;
 
-            while (isExit == false)
+            float courseUsd = 76;
+            float courseEur = 89;
+            float courseUa = 2;
+            float correncyCount;
+
+            bool isExid = false;
+            bool isEnoughMmaney;
+            int selectionExchangeCurrency;
+
+            Console.WriteLine("Здравствуйте нашем обменнике вы можете поменять: \n" +
+                              "Рубли на доллары для этого нажмите 1 \n" +
+                              "Рубли на евро  для этого нажмите 2 \n" +
+                              "Рубли на гривны  для этого нажмите 3 \n" +
+                              "Для выхода нажмите 4\n\n\n");
+
+            while (isExid == false)
             {
-                Console.WriteLine($"Ваше сгенерированное число: {random.Next(0, 100)}");
+                Console.WriteLine("Ведите какую валюту вы хотите поменять ?");
+                selectionExchangeCurrency = Convert.ToInt32(Console.ReadLine());
 
-                userInput = Console.ReadLine();
-                if (userInput == "exit")
+                Console.WriteLine("Ведите сумму которую хотите обменять: ");
+                correncyCount = Convert.ToInt32(Console.ReadLine());
+                
+
+                switch (selectionExchangeCurrency)
                 {
-                    isExit = true;
+                    case 1:
+                        isEnoughMmaney =  walletRub > (courseUsd * correncyCount);
+                        if (isEnoughMmaney)
+                        {
+                            walletUsd += correncyCount;
+                            walletRub -= correncyCount * courseUsd;
+                        }
+                        else
+                        {
+                            Console.WriteLine("К сожалению вам не хватает денег");
+                        }
+                        break;
+                    case 2:
+                        isEnoughMmaney = walletRub > (courseEur * correncyCount);
+                        if (isEnoughMmaney)
+                        {
+                            walletEur += correncyCount;
+                            walletRub -= correncyCount * courseEur;
+                        }
+                        else
+                        {
+                            Console.WriteLine("К сожалению вам не хватает денег");
+                        }
+                        break;
+                    case 3:
+                        isEnoughMmaney = walletRub > (courseUa * correncyCount);
+                        if (isEnoughMmaney)
+                        {
+                            walletUa += correncyCount;
+                            walletRub -= correncyCount * courseUa;
+                        }
+                        else
+                        {
+                            Console.WriteLine("К сожалению вам не хватает денег");
+                        }
+
+                        break;
+                    case 4:
+                        isExid = true;
+                        break;
+
                 }
+                Console.WriteLine($"На вашему кошельку {walletUsd} долларов {walletEur} евро и {walletUa} гривен и {walletRub} рублей");
             }
+
+
+            Console.ReadLine();
         }
     }
 }
