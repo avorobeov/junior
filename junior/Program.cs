a40e965a3005c8667d9ce7992167117b28745f38
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-01-27 21:28:29 +0300

    dev 1

--- junior/Program.cs
+++ junior/Program.cs
@@ -11,25 +11,29 @@ namespace junior
 
         static void Main(string[] args)
         {
-            int[,] arrayNumber = { { 1, 1, 1, 1, 1, 1 }, { 2, 2, 2, 2, 2, 2 } };
-            int[] arraySumNumbers = new int[arrayNumber.GetLength(0)];
+            int[,] arrayNumber = {{1, 2, 3},
+                                  {4, 5, 6},
+                                  {7, 8, 9}};
+           
             int sumNumbers = 0;
+            int firstColumnProduct = arrayNumber[0, 0];
+
             for (int i = 0; i < arrayNumber.GetLength(0); i++)
             {
-                for (int j = 0; j < arrayNumber.GetLength(1); j++)
+                for (int j = 1; j <= 1; j++)
                 {
-                    Console.Write(arrayNumber[i, j]);
+                    Console.Write(arrayNumber[i, j] + " ");
                     sumNumbers += arrayNumber[i, j];
-                    arraySumNumbers[i] = sumNumbers;
                 }
                 Console.WriteLine();
-                sumNumbers = 0;
             }
 
-            for (int i = 0; i < arraySumNumbers.Length; i++)
+            for (int i = 1; i < arrayNumber.GetLength(0); i++)
             {
-                Console.WriteLine($"Сума {i} столбца равняется  {arraySumNumbers[i]}");
+                firstColumnProduct *= arrayNumber[i, 0];
+                Console.WriteLine();
             }
+            Console.WriteLine($"Сума второй строки: {sumNumbers} произведение первого столбца {firstColumnProduct}");
             Console.ReadKey();
 
 
