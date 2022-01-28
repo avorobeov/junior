82234cdbef1e36ee9edb7b59ef790b4e2f780f60
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-01-28 15:54:22 +0300

    dev 1

--- junior/Program.cs
+++ junior/Program.cs
@@ -11,28 +11,35 @@ namespace junior
 
         static void Main(string[] args)
         {
-            int[,] arrayNumber = {{1, 2, 3},
-                                  {4, 5, 6},
-                                  {7, 8, 9}};
+            int[,] numbers = {{6, 2, 3, 4 ,5, 6, 7, 8, 9},
+                              {3, 2, 3, 4 ,5, 6, 7, 8, 9},
+                              {4, 2, 3, 4 ,5, 6, 7, 8, 9},
+                              {3, 2, 3, 4 ,5, 6, 7, 8, 9},
+                              {8, 2, 3, 4 ,5, 6, 7, 8, 9}};
 
-            int indexBeforeCastElement = arrayNumber.GetLength(0) - 1;
             int sumNumbers = 0;
             int firstColumnProduct = 0;
 
-            for (int i = 0; i < arrayNumber.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                for (int j = 0; j < arrayNumber.GetLength(0); j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    Console.Write(arrayNumber[i, j]);
+                    Console.Write(numbers[i, j]);
                 }
-                sumNumbers += arrayNumber[i, 1];
-
                 Console.WriteLine();
             }
 
-            for (int i = 0; i < indexBeforeCastElement;)
+            for (int j = 0; j < numbers.GetLength(1); j++)
             {
-                firstColumnProduct = arrayNumber[i, 0] * arrayNumber[++i, 0];
+                Console.Write(numbers[1, j]);
+                sumNumbers += numbers[1, j];
+            }
+            Console.WriteLine();
+
+            firstColumnProduct = numbers[0, 0];
+            for (int i = 1; i < numbers.GetLength(0); i++)
+            {
+                firstColumnProduct *= numbers[i, 0];
             }
             Console.WriteLine($"Сума второй строки: {sumNumbers} произведение первого столбца {firstColumnProduct}");
             Console.ReadKey();
