489ecf6c3b6d9885e5de7f0f105d60f43eb71b8e
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-01-29 19:33:06 +0300

    dev 1

--- junior/Program.cs
+++ junior/Program.cs
@@ -11,53 +11,159 @@ namespace junior
 
         static void Main(string[] args)
         {
-            int[] numbers = new int[0];
-            string usersCommand = "";
+            string[] userData = new string[0];
+            string[] userPosition = new string[0];
+
+            string userInput;
+
             bool isExit = false;
 
             while (isExit == false)
             {
-                Console.WriteLine("Ведите число или команду \n " +
-                                  "1)Считает сумму введенных вами чисел (sum) \n " +
-                                  "2)Выход из программы (exit) \n " +
-                                  "3)Для добавления чисел для подсчёта просто ведите число \n\n\n");
+                Console.WriteLine("          Меню \n" +
+                                  "1) Добавить досье\n" +
+                                  "2) Вывести все досье\n" +
+                                  "3) Удалить досье\n" +
+                                  "4) Поиск по фамилии\n" +
+                                  "5) Выход");
+                userInput = Console.ReadLine();
 
-                usersCommand = Console.ReadLine();
-                switch (usersCommand)
+                Console.Clear();
+
+                switch (Convert.ToInt32(userInput))
                 {
-                    case "sum":
-                        int sumNumbers = 0;
-                       
-                        foreach (int number in numbers)
-                        {
-                            sumNumbers += number;
-                        }
-                       
-                        Console.WriteLine($"Сумма всех чисел {sumNumbers}\n");
-                        sumNumbers = 0;
-                        break;
-                    case "exit":
-                        isExit = true;
-                        break;
-                    default:
-                        int[] arrayExpander = new int[numbers.Length + 1];
+                    case 1:
+                        Console.Write("Ведите свои данные (Фамилию Имя Отчество): ");
+                        userInput = Console.ReadLine();
+                        ArrayExpansion(ref userData);
+                        userData[userData.Length - 1] = userInput;
 
-                        if (numbers.Length != 0)
+                        Console.Write("Ведите должность: ");
+                        userInput = Console.ReadLine();
+                        ArrayExpansion(ref userPosition);
+                        userPosition[userData.Length - 1] = userInput;
+                        break;
+                    case 2:
+                        if (userData.Length > 0 && userPosition.Length > 0)
                         {
-                            for (int i = 0; i < numbers.Length; i++)
-                            {
-                                arrayExpander[i] = numbers[i];
-                            }
-                            arrayExpander[arrayExpander.Length - 1] = Convert.ToInt32(usersCommand);
-                            numbers = arrayExpander;
+                            Console.WriteLine("Вывожу все досье");
+                            DataOutput(userData, userPosition);
                         }
                         else
                         {
-                            arrayExpander[0] = Convert.ToInt32(usersCommand);
-                            numbers = arrayExpander;
+                            WrateError(1);
                         }
                         break;
+                    case 3:
+                        if (userData.Length > 0 && userPosition.Length > 0)
+                        {
+                            Console.Write("Ведите номер досье которое  хотите удалить: ");
+                            userInput = Console.ReadLine();
+                            DossierDeletion(ref userData, Convert.ToInt32(userInput));
+                            DossierDeletion(ref userPosition, Convert.ToInt32(userInput));
+                        }
+                        else
+                        {
+                            WrateError(1);
+                        }
+                        break;
+                    case 4:
+                        if (userData.Length > 0 && userPosition.Length > 0)
+                        {
+                            int index;
+                            Console.Write("Ведите фамилию того человека которого хотите найти:");
+                            userInput = Console.ReadLine();
+
+                            index = StringSearch(userData, userInput);
+
+                            if (index > -1)
+                            {
+                                DataOutput(userData, userPosition, index);
+                            }
+                            else
+                            {
+                                WrateError(2);
+                            }
+                        }
+                        else
+                        {
+                            WrateError(1);
+                        }
+                        break;
+                    case 5:
+                        isExit = true;
+                        break;
                 }
+            }
+        }
+        static void ArrayExpansion(ref string[] array)
+        {
+            string[] arrayGrowth = new string[array.Length + 1];
+
+            if (array.Length != 0)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    arrayGrowth[i] = array[i];
+                }
+            }
+            array = arrayGrowth;
+        }
+        static void DataOutput(string[] data, string[] position)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine($"{i}) {data[i]}-{position[i]}");
+            }
+        }
+        static void DataOutput(string[] data, string[] position, int index)
+        {
+            Console.WriteLine($"{index}) {data[index]}-{position[index]}");
+        }
+        static void DossierDeletion(ref string[] dossier, int index)
+        {
+            string[] reducedArray;
+            reducedArray = new string[dossier.Length - 1];
+            for (int i = 0; i < dossier.Length; i++)
+            {
+                if (index != i)
+                {
+                    if (reducedArray.Length < i)
+                    {
+                        reducedArray[i] = dossier[i];
+                    }
+                    else
+                    {
+                        reducedArray[i - 1] = dossier[i];
+                    }
+                }
+            }
+            dossier = reducedArray;
+        }
+        static int StringSearch(string[] array, string searchLine)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                foreach (string str in array[i].Split(' '))
+                {
+                    if (searchLine == str)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        static void WrateError(int indexError)
+        {
+            switch (indexError)
+            {
+                case 1:
+                    Console.WriteLine("Нет досье в базе:");
+                    break;
+                case 2:
+                    Console.WriteLine("Такого пользователя нет в системе: ");
+                    break;
             }
         }
     }
