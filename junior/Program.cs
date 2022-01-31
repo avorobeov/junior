26302461e06137ba5b46105f9b2a3a89e6d1ed81
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-01-31 21:11:40 +0300

    dev 1

--- junior/Program.cs
+++ junior/Program.cs
@@ -11,47 +11,103 @@ namespace junior
 
         static void Main(string[] args)
         {
-            string userInput;
-            int numberСonverted =0;
+            Console.CursorVisible = false;
+            int personX, personY;
+            int personDX = 0, personDY = 0;
+            string userInputMap;
+            char[,] map;
 
-            bool isExit = false;
-            bool isNumber;
+            Console.Write("Ведите название карты:");
+            userInputMap = Console.ReadLine();
+            Console.Clear();
 
-            while (isExit  == false)
+            map = ReadMap(userInputMap, out personX,out personY);
+
+            bool isPlaying = true;
+            DrawMap(map);
+
+            while (isPlaying == true)
             {
-                Console.Write("Ведите число для конвертации:");
-                userInput = Console.ReadLine();
-
-                isNumber = ConvertNumber(userInput, ref numberСonverted);
-
-                if (isNumber)
+                if (Console.KeyAvailable)
                 {
-                    Console.WriteLine($"Строку получилось конвертировать вот её значение {numberСonverted}");
-                    isExit = true;
-                }
-                else
-                {
-                    Console.WriteLine("Конвертация не удалось вести строку ещё раз");
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            personDX = -1;
+                            personDY = 0;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            personDX = +1;
+                            personDY = 0;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            personDX = 0;
+                            personDY = -1;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            personDX = 0;
+                            personDY = +1;
+                            break;
+                        case ConsoleKey.Escape:
+                            isPlaying = false;
+                            break;
+                    }
+                    if (map[personX + personDX, personY + personDY] != '|')
+                    {
+                        Console.SetCursorPosition(personY, personX);
+                        Console.Write(" ");
+
+                        personX += personDX;
+                        personY += personDY;
+
+                        Console.SetCursorPosition(personY, personX);
+                        Console.Write("@");
+                    }
                 }
             }
-            Console.ReadLine();
+
+
+            Console.ReadKey();
+
         }
-        static bool ConvertNumber(string inputString, ref int outputNumber )
+        static void DrawMap(char[,] map)
         {
-            int number;
-            bool isNumber;
-
-            isNumber = int.TryParse(inputString, out number);
-          
-            if (isNumber)
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                outputNumber = number;
-                return true;
-            }
-            else
-            {
-                return false;
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
             }
         }
+        static char[,] ReadMap(string nameMap,out int personX,out int personY)
+        {
+            personX = 0;
+            personY = 0;
+            string [] newFile = System.IO.File.ReadAllLines($"Maps\\{nameMap}.txt");
+            char[,] map = new char[newFile.Length, newFile[0].Length];
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+
+                    if (newFile[i][j] == '%')
+                    {
+                        personX = i;
+                        personY = j;
+                        map[i, j] = '@';
+                    }
+                    else
+                    {
+                        map[i, j] = newFile[i][j];
+                    }
+                }
+            }
+            return map;
+        }
+       
     }
 }
---
+++ junior/Program2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace junior
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string userInput;
+            int numberСonverted =0;
+
+            bool isExit = false;
+            bool isNumber;
+
+            while (isExit  == false)
+            {
+                Console.Write("Ведите число для конвертации:");
+                userInput = Console.ReadLine();
+
+                isNumber = ConvertNumber(userInput, ref numberСonverted);
+
+                if (isNumber)
+                {
+                    Console.WriteLine($"Строку получилось конвертировать вот её значение {numberСonverted}");
+                    isExit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Конвертация не удалось вести строку ещё раз");
+                }
+            }
+            Console.ReadLine();
+        }
+        static bool ConvertNumber(string inputString, ref int outputNumber )
+        {
+            int number;
+            bool isNumber;
+
+            isNumber = int.TryParse(inputString, out number);
+          
+            if (isNumber)
+            {
+                outputNumber = number;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
