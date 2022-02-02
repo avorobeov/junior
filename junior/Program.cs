1520a315b05e7a8e88479c7241066a0a7bec396a
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-02-02 13:48:05 +0300

    dev 1

--- junior/Program.cs
+++ junior/Program.cs
@@ -14,7 +14,7 @@ namespace junior
             Dictionary<string, string> talkDictionary = new Dictionary<string, string>();
 
             string inputUser;
-
+            string queryResults;
             talkDictionary.Add("слово", "Единица языка, служащая длянаименования понятий, предметов, лиц, действий, состояний, признаков,связей, отношений, оценок. Знаменательные и служебные слова. Происхождениеслов. С. в с. (о переводе, пересказе: буквально).");
             talkDictionary.Add("результат", "РЕЗУЛЬТАТ, -а, м. 1. То, что получено в завершение какой-н.деятельности, работы, итог. Результаты исследования. Результаты конкурса. 2.Показатель мастерства (обычно спортивного). Р. пловца. Р. в беге \"а 100м.Улучшить свои результаты.Лучшийр.дня. * В результате чего, предлог с род.п. - вследствие чего - н., из - за чего - н.Ущерб в результате аварии.Пострадалв результате неосторожности.В результате того что, союз(книжн.) - вследствие того что, из - за того что.Всегда опаздывает, в результате тогочто не может рассчитать время.А(и) в результате, в знач.союза - и потому, и вследствие этого.Не рассчитали время, а(и) в результате опоздали.IIприл.результатный, -ая, -ое.");
 
@@ -32,23 +32,24 @@ namespace junior
                         isExit = true;
                         break;
                     default:
-                        SeekMeaning(talkDictionary, inputUser);
+                        queryResults = SeekMeaning(talkDictionary, inputUser);
+                        Console.WriteLine(queryResults);
                         break;
                 }
             }
         }
 
-        static void SeekMeaning(Dictionary<string, string> Dictionary, string input)
+        static string SeekMeaning(Dictionary<string, string> Dictionary, string input)
         {
             foreach (var item in Dictionary)
             {
                 if (item.Key.ToLower() == input.ToLower())
                 {
-                    Console.WriteLine($"Ответ на ваш запрос: {item.Value}");
+                    return $"Ответ на ваш запрос: {item.Value}";
                 }
             }
 
-            Console.WriteLine("Такого слова нет в базе: ");
+            return "Такого слова нет в базе: ";
         }
 
 
