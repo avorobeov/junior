d1afa80a3fd2ebe6eea1cba6b5d1a4f9ee93dc35
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-02-02 16:43:06 +0300

    dev 1

--- junior/Program.cs
+++ junior/Program.cs
@@ -11,44 +11,29 @@ namespace junior
 
         static void Main(string[] args)
         {
-            Dictionary<string, string> talkDictionary = new Dictionary<string, string>();
+            Random random = new Random();
+            Queue<int> shoppingList = new Queue<int>();
 
-            string inputUser;
-            string queryResults;
-            talkDictionary.Add("слово", "Единица языка, служащая длянаименования понятий, предметов, лиц, действий, состояний, признаков,связей, отношений, оценок. Знаменательные и служебные слова. Происхождениеслов. С. в с. (о переводе, пересказе: буквально).");
-            talkDictionary.Add("результат", "РЕЗУЛЬТАТ, -а, м. 1. То, что получено в завершение какой-н.деятельности, работы, итог. Результаты исследования. Результаты конкурса. 2.Показатель мастерства (обычно спортивного). Р. пловца. Р. в беге \"а 100м.Улучшить свои результаты.Лучшийр.дня. * В результате чего, предлог с род.п. - вследствие чего - н., из - за чего - н.Ущерб в результате аварии.Пострадалв результате неосторожности.В результате того что, союз(книжн.) - вследствие того что, из - за того что.Всегда опаздывает, в результате тогочто не может рассчитать время.А(и) в результате, в знач.союза - и потому, и вследствие этого.Не рассчитали время, а(и) в результате опоздали.IIприл.результатный, -ая, -ое.");
-
-            bool isExit = false;
-
-            while (isExit == false)
+            int amountFunds = 0;
+            for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine("Для выхода ведите команду Exit");
-                Console.WriteLine("Ведите слово для получения его значения:");
-                inputUser = Console.ReadLine();
-
-                switch (inputUser)
-                {
-                    case "Exit":
-                        isExit = true;
-                        break;
-                    default:
-                        queryResults = SeekMeaning(talkDictionary, inputUser);
-                        Console.WriteLine(queryResults);
-                        break;
-                }
+                shoppingList.Enqueue(random.Next(50, 1000));
             }
-        }
 
-        static string SeekMeaning(Dictionary<string, string> Dictionary, string querySearch)
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                amountFunds += shoppingList.Dequeue();
+                WrateResult(amountFunds, shoppingList.Count);
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+        static void WrateResult(int balance, int countPeopleLine)
         {
-            if (Dictionary.ContainsKey(querySearch.ToLower()))
-            {
-                return $"Ответ на ваш запрос: {Dictionary[querySearch.ToLower()]}";
-            }
-
-            return "Такого слова нет в базе: ";
+            Console.WriteLine($"На вашем счету:{balance}");
+            Console.WriteLine($"Количество человек в очереди: {countPeopleLine}");
         }
-
 
     }
 }
