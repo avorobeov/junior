1a88d2ae223b3853e5c8ee683fb6c41345b72d4c
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-02-06 17:56:43 +0300

    dev 1

--- junior/Program.cs
+++ junior/Program.cs
@@ -16,7 +16,7 @@ namespace junior
 
             Knight knight = new Knight();
             Renderer renderer = new Renderer();
-      
+
             knight.SetPosition(10, 10);
 
             knightPositionX = knight.PositionX;
@@ -26,70 +26,70 @@ namespace junior
 
             Console.ReadLine();
         }
-
-        class Renderer
+    }
+    class Renderer
+    {
+        public void DrawPlayer(int positionX, int positionY, char character = '$')
         {
-            public void DrawPlayer(int positionX, int positionY, char character = '$')
-            {
-                Console.SetCursorPosition(positionX, positionY);
-                Console.Write(character);
-            }
-        }
-        class Knight
-        {
-            private int _health;
-            private int _armor;
-            private int _damage;
-
-            private int _positionX;
-            private int _positionY;
-
-            public int PositionX
-            {
-                get
-                {
-                    return _positionX;
-                }
-                private set
-                {
-                    _positionX = value;
-                }
-            }
-            public int PositionY
-            {
-                get
-                {
-                    return _positionY;
-                }
-                private set
-                {
-                    _positionY = value;
-                }
-            }
-            public Knight(int health, int armor, int damage)
-            {
-                _health = health;
-                _armor = armor;
-                _damage = damage;
-            }
-            public Knight()
-            {
-                _health = 150;
-                _armor = 30;
-                _damage = 25;
-            }
-            public void ShowInfo()
-            {
-                Console.WriteLine($"Количество здоровья {_health} \n" +
-                                  $"Количество брони {_armor}\n" +
-                                  $"Количество урона {_damage}");
-            }
-            public void SetPosition(int setPositionX, int SetPositionY)
-            {
-                PositionX = setPositionX;
-                PositionY = SetPositionY;
-            }
+            Console.SetCursorPosition(positionX, positionY);
+            Console.Write(character);
         }
     }
+    class Knight
+    {
+        private int _health;
+        private int _armor;
+        private int _damage;
 
+        private int _positionX;
+        private int _positionY;
+
+        public int PositionX
+        {
+            get
+            {
+                return _positionX;
+            }
+       
+            private set
+            {
+                _positionX = value;
+            }
+        }
+        public int PositionY
+        {
+            get
+            {
+                return _positionY;
+            }
+        
+            private set
+            {
+                _positionY = value;
+            }
+        }
+        public Knight(int health, int armor, int damage)
+        {
+            _health = health;
+            _armor = armor;
+            _damage = damage;
+        }
+        public Knight()
+        {
+            _health = 150;
+            _armor = 30;
+            _damage = 25;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Количество здоровья {_health} \n" +
+                              $"Количество брони {_armor}\n" +
+                              $"Количество урона {_damage}");
+        }
+        public void SetPosition(int positionX, int positionY)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+    }
 }
